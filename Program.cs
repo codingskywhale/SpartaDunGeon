f80c290f3c5ad2cbda6ce50fa7a1ea4f78e8c631
codingskywhale <167049910+codingskywhale@users.noreply.github.com>
2024-04-30 16:59:16 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿public class Manager
+{
+    internal void StartGame()
+    {
+        MainMenu();
+    }
+
+    private void MainMenu()
+    {
+        Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+        Console.WriteLine("이제 전투를 시작할 수 있습니다.\n");
+
+        Console.WriteLine("1. 상태 보기");
+        Console.WriteLine("2. 전투 시작\n");
+
+        Console.Write("원하시는 행동을 입력해 주세요.\n>>");
+
+        switch (consoleUtility.PromptMenuChoise(1, 2))
+        {
+            case 1:
+                Console.WriteLine("상태 보기");
+                break;
+            case 2:
+                Console.WriteLine("전투 시작");
+                break;
+        }
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Manager manager = new Manager();
+        manager.StartGame();
+    }
+}
---
+++ consoleUtility.cs
@@ -0,0 +1,18 @@
+﻿internal class consoleUtility
+{
+    internal static int PromptMenuChoise(int min, int max)
+    {
+        while (true)
+        {
+            if(int.TryParse(Console.ReadLine(), out int choise) && choise>=min && choise <= max)
+            {
+                return choise;
+            }
+            else
+            {
+                Console.Write("잘못된 입력입니다. 원하시는 선택지의 숫자를 입력하세요.\n>>");
+
+            }
+        }
+    }
+}
