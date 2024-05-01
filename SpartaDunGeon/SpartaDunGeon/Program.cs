da0f0786306bfb905aa21a909ad5b55dec35ee55
codingskywhale <167049910+codingskywhale@users.noreply.github.com>
2024-05-01 11:44:38 +0900

    중간 저장
    중간 저장

--- SpartaDunGeon/SpartaDunGeon/Program.cs
+++ SpartaDunGeon/SpartaDunGeon/Program.cs
@@ -19,6 +19,7 @@
             {
                 Console.WriteLine("전사를 선택하셨습니다.");
                 MainMenu();
+                return;
             }
             else if (jobChoise == "2")
             {
@@ -35,7 +36,53 @@
     private void MainMenu()
     {
         Console.Clear();
-        Console.WriteLine("안녕하세요.");
+        //인트로
+        Console.WriteLine("##############################");
+        Console.WriteLine("스파르타 게임을 시작한다");
+        Console.WriteLine("원하는 행동을 고르세요.");
+        Console.WriteLine("##############################\n");
+
+        //선택지
+        Console.WriteLine("1. 상태창");
+        Console.WriteLine("2. 장비창");
+        Console.WriteLine("3. 상점");
+        Console.WriteLine("4. 전투 시작");
+        Console.Write("\n원하는 선택지를 고르세요: ");
+
+        //선택지 검증
+        int Choise = ConsoleUtility.PromptMenuChoise(1, 4);
+
+        //메뉴 중에서 선택
+        switch (Choise)
+        {
+            case 1:
+                StateMenu();
+                break;
+            case 2:
+                InventoryMenu();
+                break;
+            case 3:
+                StoreMenu();
+                break;
+            case 4:
+                Console.WriteLine("전투 시작");
+                break;
+        }
+    }
+
+    private void StateMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void InventoryMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StoreMenu()
+    {
+        throw new NotImplementedException();
     }
 
     internal class Program
