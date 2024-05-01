cc2658aa2c0f76729df380cbcb60cb0e2b47732f
codingskywhale <167049910+codingskywhale@users.noreply.github.com>
2024-05-01 16:42:42 +0900

    일부 색상 수정
    일부 문구와 강조 색상 수정

--- SpartaDunGeon/SpartaDunGeon/Program.cs
+++ SpartaDunGeon/SpartaDunGeon/Program.cs
@@ -18,7 +18,9 @@ public class GameManager
         Console.Write("이름을 입력해 주세요: ");
         string name = Console.ReadLine();
 
-        Console.WriteLine($"입력하신 이름은 '{name}' 입니다.");
+        Console.Clear();
+
+        ConsoleUtility.PrintTextHighlight(Color.Yellow,"입력하신 이름은 '",name,"' 입니다.\n");
         Console.WriteLine("1. 예");
         Console.WriteLine("2. 아니오\n");
 
@@ -40,7 +42,7 @@ public class GameManager
     {
         Console.Clear ();
 
-        Console.WriteLine("직업을 선택해 주세요.");
+        Console.WriteLine("직업을 선택해 주세요.\n");
         Console.WriteLine("1. 전사");
         Console.WriteLine("2. 마법사");
         Console.Write("\n>>");
@@ -50,17 +52,19 @@ public class GameManager
             string jobChoise = Console.ReadLine();
             if (jobChoise == "1")
             {
-                Console.WriteLine("전사를 선택하셨습니다.");
+                Console.Clear() ;
+                ConsoleUtility.PrintTextHighlight(Color.Red,"당신은 용맹한 ","전사","를 선택하셨습니다.");
                 player = new Player(name, "전사", 1, 10, 7, 70, 70, 700);
-                Thread.Sleep(1000);
+                Thread.Sleep(2000);
                 MainMenu();
                 break;
             }
             else if (jobChoise == "2")
             {
-                Console.WriteLine("마법사를 선택하셨습니다.");
+                Console.Clear();
+                ConsoleUtility.PrintTextHighlight(Color.Blue,"당신은 현명한 ","마법사", "를 선택하셨습니다.");
                 player = new Player(name, "마법사", 1, 5, 5, 50, 50, 500);
-                Thread.Sleep(1000);
+                Thread.Sleep(1500);
                 MainMenu();
                 break;
             }
@@ -85,7 +89,7 @@ public class GameManager
         Console.WriteLine("1. 상태창");
         Console.WriteLine("2. 장비창");
         Console.WriteLine("3. 상점");
-        Console.WriteLine("4. 전투 시작");
+        Console.WriteLine("4. 전투 시작\n");
 
         //선택지 검증
         int Choise = ConsoleUtility.ChoiceMenu(1, 4);
