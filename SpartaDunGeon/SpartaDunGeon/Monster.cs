b0504d82a791c2e16cc135476dd4956730510b81
codingskywhale <167049910+codingskywhale@users.noreply.github.com>
2024-05-01 15:29:25 +0900

    인덱스
    인덱스 추가

--- SpartaDunGeon/SpartaDunGeon/Monster.cs
+++ SpartaDunGeon/SpartaDunGeon/Monster.cs
@@ -2,16 +2,18 @@
 {
     public List<Monster> monsters;
     public string Name { get; set; }
-    public string Job { get; }
+    public int Idx { get; }
     public int Lv { get; }
     public int Atk { get; }
     public int Def { get; }
     public int Hp { get; }
     public int Gold { get; set; }
 
-    public Monster(string name, int lv, int atk, int def, int hp, int gold)
+    public Monster( string name, int idx, int lv, int atk, int def, int hp, int gold)
     {
+
         Name = name;
+        Idx = idx;
         Lv = lv;
         Atk = atk;
         Def = def;
@@ -23,10 +25,10 @@
     {
         monsters = new List<Monster>();
 
-        monsters.Add(new Monster("슬라임", 1, 1, 1, 3, 5));
-        monsters.Add(new Monster("고블린", 2, 2, 1, 5, 10));
-        monsters.Add(new Monster("코볼트", 3, 7, 3, 10, 30));
-        monsters.Add(new Monster("오크", 5, 10, 5, 20, 50));
-        monsters.Add(new Monster("드래곤", 20, 20, 20, 100, 200));
+        monsters.Add(new Monster("슬라임",0 , 1, 1, 1, 3, 5));
+        monsters.Add(new Monster("고블린",1 , 2, 2, 1, 5, 10));
+        monsters.Add(new Monster("코볼트",2 , 3, 7, 3, 10, 30));
+        monsters.Add(new Monster("오크", 3, 5, 10, 5, 20, 50));
+        monsters.Add(new Monster("드래곤", 4, 20, 20, 20, 100, 200));
     }
 }
