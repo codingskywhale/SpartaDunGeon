b7800930ec79272194a6c6be23d61dbd4da65cfb
Github <Github>
2024-05-01 14:12:40 +0900

    Create Item.cs
    Item.cs 생성
    class Item 생성
    필드 및 생성자 작성

---
+++ SpartaDunGeon/SpartaDunGeon/Item.cs
@@ -0,0 +1,34 @@
+﻿namespace SpartaDunGeon
+{
+    public enum ItemType
+    {
+        WEAPON,
+        ARMOR,
+        POTION
+    }
+    internal class Item
+    {
+        public string Name { get; }
+        public string Desc { get; }
+
+        public ItemType Type;
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int Price { get; }
+        public bool IsEquipped { get; private set; }
+        public bool IsPurchased { get; private set; }
+        public Item(string name, string desc, ItemType type, int atk, int def, int hp, int price, bool isEquipped = false, bool isPurchased = false)
+        {
+            Name = name;
+            Desc = desc;
+            Type = type;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Price = price;
+            IsEquipped = isEquipped;
+            IsPurchased = isPurchased;
+        }
+    }
+}
