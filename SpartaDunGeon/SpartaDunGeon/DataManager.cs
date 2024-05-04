ef2c58cc9e3833d80b82c7fcf4a8374c7989d178
Github <Github>
2024-05-04 12:05:13 +0900

    Create DataManager.cs
    데이터 매니저 추가

---
+++ SpartaDunGeon/SpartaDunGeon/DataManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Spartadungeon;
+
+namespace SpartaDunGeon
+{
+    internal class DataManager
+    {
+        public static DataManager _player = new DataManager();
+        public Player PlayerData { get; set; }
+        public List<Item> InventoryData { get; set; }
+        public List<Item> StoreData { get; set; }
+        public List<Item> PotionData { get; set; }
+        public List<Quest> QuestListData { get; set; }
+        public StageData StageData { get; set; }
+
+        public static bool Data(string SoL)
+        {
+            string path = Path.GetFullPath("./SaveData.json");
+            if (SoL == "Save")
+            {
+                _player = new DataManager
+                {
+                    PlayerData = GameManager.player,
+                    InventoryData = Inventory.inventory,
+                    StoreData = Store.storeInventory,
+                    PotionData = Inventory.potionInventory,
+                    QuestListData = QuestManager.questList,
+                    StageData = Dungeon.stage
+                };
+                string saveData = JsonConvert.SerializeObject(_player, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(path, saveData);
+                return true;
+            }
+            else if (SoL == "Load")
+            {
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("저장된 데이터가 없습니다.");
+                    Thread.Sleep(500);
+                    return false;
+                }
+                string LoadData = File.ReadAllText(Path.GetFullPath("./SaveData.json"));
+                _player = JsonConvert.DeserializeObject<DataManager>(LoadData);
+                GameManager.player = _player.PlayerData;
+                Inventory.inventory = _player.InventoryData;
+                Store.storeInventory = _player.StoreData;
+                Inventory.potionInventory = _player.PotionData;
+                QuestManager.questList = _player.QuestListData;
+                Dungeon.stage = _player.StageData;
+                return true;
+            }
+            return false;
+        }
+    }
+}
